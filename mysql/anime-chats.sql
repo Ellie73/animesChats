26aec189bbb2b48acf680bcecd4daac88139a51a
DanelLima <128085169+DanelLima@users.noreply.github.com>
2023-05-07 21:43:30 -0300

    Add files via upload
    banco de dados

--- mysql/anime-chats.sql
+++ mysql/anime-chats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Maio-2023 às 20:44
+-- Tempo de geração: 08-Maio-2023 às 02:42
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -20,6 +20,51 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `anime-chats`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `chat`
+--
+
+CREATE TABLE `chat` (
+  `idchat` int(11) NOT NULL,
+  `tema` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `mensagem`
+--
+
+CREATE TABLE `mensagem` (
+  `idmensagem` int(11) NOT NULL,
+  `conteudo` varchar(255) NOT NULL,
+  `remetente` int(11) NOT NULL,
+  `chat` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tema`
+--
+
+CREATE TABLE `tema` (
+  `idtema` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `sinopse` varchar(500) NOT NULL,
+  `genero` varchar(100) NOT NULL,
+  `ano_estreia` int(4) NOT NULL,
+  `quantidade` int(5) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `tipotema` varchar(50) NOT NULL,
+  `fototema` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,11 +92,30 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `senha`, `estado`, `cidade`, `genero`, `perfil`, `telefone`, `foto`, `nascimento`, `situacaoUsuario`) VALUES
-(2, 'Daniel Souza de Lima', 'danielqualquercoisa25@gmail.com', 'animal', 'df', 'df', 'M', 'U', 2147483647, '../img/usuarios/user.png', '2023-04-26', 1);
+(3, 'Daniel Souza de Lima', 'danielqualquercoisa25@gmail.com', 'sadgfdhgj', 'df', 'df', 'M', 'U', 0, '../img/usuarios/user.png', '2023-05-04', 1),
+(4, 'Daniel Souza de Lima', 'danielqualquercoisa25@gmail.com', 'e8d95a51f3af4a3b134bf6bb680a213a', 'df', 'df', 'M', 'U', 0, '../img/usuarios/user.png', '2023-05-01', 1);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`idchat`);
+
+--
+-- Índices para tabela `mensagem`
+--
+ALTER TABLE `mensagem`
+  ADD PRIMARY KEY (`idmensagem`);
+
+--
+-- Índices para tabela `tema`
+--
+ALTER TABLE `tema`
+  ADD PRIMARY KEY (`idtema`);
 
 --
 -- Índices para tabela `usuario`
@@ -64,10 +128,28 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de tabela `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `idchat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `mensagem`
+--
+ALTER TABLE `mensagem`
+  MODIFY `idmensagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tema`
+--
+ALTER TABLE `tema`
+  MODIFY `idtema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
